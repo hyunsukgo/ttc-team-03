d3b3e8d9b39a164f76aa826d1d0905eb657d3004
Hyunseok <admin@example.com>
2020-09-06 11:06:27 +0900

    remove Docker file

--- Web/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -f /var/www/html/wp-config.php ]; then
-
-chown www-data:www-data /var/www/html/wp-config.php
-chown www-data:www-data /var/www/html/
-chmod 666 /var/www/html/wp-config.php
-
-fi
